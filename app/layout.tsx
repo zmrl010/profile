45521de94e3a8a6aec25555b4c6182f79d803a19
Zach <zmriel@gmail.com>
2023-01-11 12:34:13 -0500

    chore: cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import './globals.css'
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout(props: Props) {
   return (
     <html lang="en">
       {/*
@@ -12,7 +12,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>{props.children}</body>
     </html>
-  )
+  );
 }
